c9b776bade93d2c938902b0186fd84585a6988dc
Gordon <1029052540@qq.com>
2019-02-19 23:57:52 +0800

    <feat>: add user usercase

--- usecase/user.go
+++ usecase/user.go
@@ -3,6 +3,8 @@ package usecase
 import (
 	"context"
 	"github.com/QianPai/account/model"
+	"github.com/QianPai/account/repository"
+	"time"
 )
 
 type User interface {
@@ -11,4 +13,29 @@ type User interface {
 	ResetPassword(ctx context.Context) (res model.User)
 	ResetToken(ctx context.Context) (res model.User)
 	GetSmsCheckCode(ctx context.Context) string
+}
+
+type userUsecase struct {
+	userRepo repository.User
+	contextTimeout time.Duration
+}
+
+func NewUserUsecase(repo repository.User, timeout time.Duration) *userUsecase {
+	return &userUsecase{
+		userRepo: repo,
+		contextTimeout: timeout,
+	}
+}
+
+func (u userUsecase) GetById(c context.Context, id interface{}) (*model.User, error) {
+	ctx, cancel := context.WithTimeout(c, u.contextTimeout)
+	defer cancel()
+
+	res,err := u.userRepo.GetById(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, err
+
 }
